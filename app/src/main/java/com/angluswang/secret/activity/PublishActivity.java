704d03257d81eed6acc37f2099e35ab5fd4d272c
AnglusWang <AnglusWang@163.com>
2016-07-04 11:53:12 +0800

    发布消息界面设计

---
+++ app/src/main/java/com/angluswang/secret/activity/PublishActivity.java
@@ -0,0 +1,19 @@
+package com.angluswang.secret.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.angluswang.secret.R;
+
+/**
+ * Created by Jeson on 2016/7/4.
+ * 发送消息界面
+ */
+
+public class PublishActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aty_publish_message);
+    }
+}
